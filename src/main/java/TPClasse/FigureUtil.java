dc23163f2f01b3924d3d769f2045b6cbd30742cf
Julien BURTE <jburte34@gmail.com>
2025-07-16 11:39:00 +0200

    tp12

--- src/main/java/TPClasse/FigureUtil.java
+++ src/main/java/TPClasse/FigureUtil.java
@@ -2,12 +2,18 @@ package TPClasse;
 
 public class FigureUtil {
     Rond getRandomRond(int maxX, int maxY, int maxRadius) {
-        Point randomCentre = new Point((int)Math.ceil(Math.random() * maxX), (int)Math.ceil(Math.random() * maxY));
-        return new Rond(randomCentre, (int)Math.ceil(Math.random() * maxRadius));
+        return new Rond(getRandomPoint(maxX, maxY), (int)Math.ceil(Math.random() * maxRadius));
     }
 
     Rectangle getRandomRectangle(int maxX, int maxY, int maxLength, int maxHeight){
-        Point randomPoint = new Point((int)Math.ceil(Math.random() * maxX), (int)Math.ceil(Math.random() * maxY));
-        return new Rectangle(randomPoint, (int)Math.ceil(Math.random() * maxLength), (int)Math.ceil(Math.random() * maxHeight));
+        return new Rectangle(getRandomPoint(maxX, maxY), (int)Math.ceil(Math.random() * maxLength), (int)Math.ceil(Math.random() * maxHeight));
+    }
+
+    Carre getRandomCarre(int maxX, int maxY, int maxLength){
+        return  new Carre(getRandomPoint(maxX, maxY), (int)Math.ceil(Math.random() * maxLength));
+    }
+
+    Point getRandomPoint(int maxX, int maxY){
+        return new Point((int)Math.ceil(Math.random() * maxX), (int)Math.ceil(Math.random() * maxY));
     }
 }
